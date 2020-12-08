6831b7ff465a018b217dc294a760002b48720352
jean noel Ny <42876462+jeanrakitic@users.noreply.github.com>
2020-12-08 18:24:09 +0100

    added exercise.php

---
+++ exercise.php
@@ -0,0 +1,46 @@
+<head>
+<meta http-equiv="refresh" content="10" />
+<title>Jean-Noel  Nyirisheja</title>
+
+</head>
+
+<body>
+<?php
+
+
+$dbhost="remotemysql.com "; $dbuser="phzp2UddPb"; $dbpassword="HQjL16d2AX"; $dbname="phzp2UddPb";
+$connection = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbname);
+if (!$connection) {
+echo " MySQL Connection error." . PHP_EOL;
+echo "Errno: " . mysqli_connect_errno() . PHP_EOL;
+echo "Error: " . mysqli_connect_error() . PHP_EOL;
+exit;
+}
+$result = mysqli_query($connection, "SELECT * FROM hosts") or die ("DB error: $dbname");
+print "<TABLE CELLPADDING=5 BORDER=1>";
+print "<TR><TD>id</TD><TD>Address</TD><TD>Port</TD><TD>Status</TD> </TR>\n";
+while ($row = mysqli_fetch_array ($result)) {
+$id = $row[0];
+$address = $row[1];
+$port = $row[2];
+print "<TR><TD>$id</TD><TD>$address</TD> <TD>$port</TD><TD>$status</TD> </TR>\n";
+
+$fp = @fsockopen($host, $port, $errno, $errstr, 30);
+echo 'Host '.$host.':'.$port.' is ';
+if ($fp) 
+{ 
+    echo $status="ok"; 
+} else 
+{ 
+    echo $status="$errstr ($errno)"; 
+}
+
+}
+
+
+mysqli_close($connection);
+
+    
+?>
+
+</body>
