3b6dd9d691e43a46dcbdfd97d7d66860e0c0f79d
jean noel Ny <42876462+jeanrakitic@users.noreply.github.com>
2021-02-05 17:40:36 +0100

    Delete test.php

--- scada/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'phplot.php';
-$data = array(array('', 10), array('', 1));
-$plot = new PHPlot();
-$plot->SetDataValues($data);
-$plot->SetTitle('First Test Plot');
-$plot->DrawGraph();
-?>
