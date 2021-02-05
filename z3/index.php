4e06234e54051c1041c268a59098f7236b084059
jean noel Ny <42876462+jeanrakitic@users.noreply.github.com>
2021-02-05 13:02:03 +0100

    Update index.php

--- z3/index.php
+++ z3/index.php
@@ -1,24 +1,13 @@
-
-<?php
-	$time = date('H:i:s', time());
-	$user  = $_POST['user'];
-	$post  = $_POST['post'];
-	if (IsSet($_POST['post'])) {
-		$text = '<table  border=”1” width="90%">
-		<tr><td>' . $post . '</td><td width="80">' . $user . '</td><td width="60" bgcolor="yellow">'. $time.'</td></tr></table><br>';
-		$file = fopen ("conversation.txt", "a+");
-		fwrite ($file, $text);
-	} 
-?>
-
-<form method="POST">
+<DOCTYPE html> 
+<html> 
+<head> 
+</head>
+<body> 
+<form method="POST" action="add.php">
 <br>
 Nick:<input type="text" name="user" maxlength="10" size="10"><br>
 Post:<input type="text" name="post" maxlength="90" size="90"><br>
 <input type="submit" value="Send"/>
+</body>
 </form>
-
-Posts:
-<br>
-<? include ("conversation.txt");?>
-<br>
+</html>
